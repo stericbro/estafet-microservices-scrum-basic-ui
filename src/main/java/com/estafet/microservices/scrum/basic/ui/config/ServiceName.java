8181551f63a36e42245d7a8a885769577d61ac0f
Steve Brown <steve.brown@estafet.com>
2019-09-19 16:36:43 +0100

    Add comments to ServiceName.

--- src/main/java/com/estafet/microservices/scrum/basic/ui/config/ServiceName.java
+++ src/main/java/com/estafet/microservices/scrum/basic/ui/config/ServiceName.java
@@ -1,23 +1,56 @@
 package com.estafet.microservices.scrum.basic.ui.config;
 
+/**
+ * Enum defining available service names.
+ * @author Dennis Williams, Etsafet Ltd.
+ *
+ */
 public enum ServiceName {
+
+    /**
+     * The project service.
+     */
 	PROJECT_SERVICE("project-api"),
+
+    /**
+     * The sprint service.
+     */
 	SPRINT_SERVICE("sprint-api"),
+
+    /**
+     * The story service.
+     */
 	STORY_SERVICE("story-api"),
+
+    /**
+     * The task service.
+     */
 	TASK_SERVICE("task-api"),
+
+    /**
+     * The health service for monitoring.
+     */
 	HEALTH_SERVICE("health-api");
-	
+
+    /**
+     * The name of the service
+     */
 	private String  serviceName;
 	
-	private ServiceName(String serviceName) {
-		this.setServiceName(serviceName);
+	/**
+	 * Construct from a service name.
+	 *
+	 * @param theServiceName
+	 *         The name of the service.
+	 */
+	private ServiceName(final String theServiceName) {
+		serviceName = theServiceName;
 	}
 
+	/**
+	 * @return the name of the service.
+	 */
 	public String getServiceName() {
 		return serviceName;
-	}
-
-	private void setServiceName(String serviceName) {
-		this.serviceName = serviceName;
 	}
 }
