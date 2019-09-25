7659664d7e43fd450a77526452aeb09a36ae17ee
Steve Brown <steve.brown@estafet.com>
2019-09-25 12:21:46 +0100

    Revert "Add comments to AcceptanceCriteriaController.java."
    This reverts commit ccfe530a529a2085b0a69e69602a25ea910fd4ad.

--- src/main/java/com/estafet/microservices/scrum/basic/ui/controllers/AcceptanceCriteriaController.java
+++ src/main/java/com/estafet/microservices/scrum/basic/ui/controllers/AcceptanceCriteriaController.java
@@ -24,42 +24,15 @@ public class AcceptanceCriteriaController {
 	@Autowired
 	private StoryService storyService;
 
-	/**
-	 * Get the mapping for the given story Id.
-	 *
-	 * <p>The {@link GetMapping} annotation maps HTTP {@code GET} "{@code /story/{id}/criteria}" requests onto this
-	 * method.</p>
-	 * @param id
-	 *         The story Id.
-	 * @param model
-	 *         The attributes for the criteria page.
-	 * @return
-	 *         The name of the criteria page, "@code criteria}" : {@code src/main/resources/templates/criteria.html}
-	 *
-	 */
 	@GetMapping("/story/{id}/criteria")
 	public String criteriaForm(@PathVariable final int id,  final Model model) {
-
-	    // Add the attributes for the {criteria} and {storyId}.
 		model.addAttribute("criteria", new AcceptanceCriterion().init());
-		model.addAttribute("storyId", Integer.valueOf(id));
+		model.addAttribute("storyId", id);
 		return "criteria";
 	}
 
-	/**
-	 * Create the POST request.
-	 * @param id
-	 *         The story Id.
-	 * @param criteria
-	 *         The acceptance criteria.
-	 * @return
-	 *         A redirect request (when {@code id} is {@code 999}) to "{@code /story/999}."
-	 */
 	@PostMapping("/story/{id}/criteria")
 	public String criteriaSubmit(@PathVariable final int id, @ModelAttribute final AcceptanceCriterion criteria) {
-
-	    // Send REST request to the story service. Returns a com.estafet.microservices.scrum.basic.ui.model.Story
-	    // object, which is not used here.
 		storyService.addAcceptanceCriteria(id, criteria);
 		return "redirect:/story/" + id;
 	}
