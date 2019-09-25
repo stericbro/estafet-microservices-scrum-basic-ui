0a846a738aecd314f29eb899cc2f6ee2c3cffbcc
Steve Brown <steve.brown@estafet.com>
2019-09-25 13:51:27 +0100

    Add class comment to verify GitHub WebHooks work.

--- src/main/java/com/estafet/microservices/scrum/basic/ui/controllers/ErrorController.java
+++ src/main/java/com/estafet/microservices/scrum/basic/ui/controllers/ErrorController.java
@@ -4,10 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * @author Dennis Williams, Estafet Ltd.
+ *
+ */
 @Controller
 @RequestMapping("/error")
 public class ErrorController {
-	
+
 	@GetMapping("/503")
 	public String error503() {
 		return "error503";
